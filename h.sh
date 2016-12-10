3f3b505a2efb906cd684135e0f3a3e58c30c84c5
Atsushi Ishida <gipcompany@gmail.com>
2016-12-11 04:30:16 +0900

    add some files

---
+++ h.sh
@@ -0,0 +1,13 @@
+_h () {
+  local cur
+  COMPREPLY=()
+  cur=${COMP_WORDS[COMP_CWORD]}
+  k=0
+  i="${HOME}/git/gipcompany/work/help" # the directory from where to start
+  for j in $( compgen -f "$i/$cur" ); do # loop trough the possible completions
+    [ -d "$j" ] && j="${j}/" || j="${j} " # if its a dir add a shlash, else a space
+    # echo $(basename ${j#$i/})
+    COMPREPLY[k++]=$(basename ${j#$i/}) # remove the directory prefix from the array
+  done
+}
+complete -o nospace -F _h h
